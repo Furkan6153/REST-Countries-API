92a76d1fbb223a0d3332191a5c9e685e394ce5ee
Furkan <furkan6153@gmail.com>
2026-05-02 23:55:52 +0300

    fix-path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
   useEffect(() => {
     const fetchCountries = async () => {
       try {
-        const response = await fetch("./data/data.json");
+        const response = await fetch(`${import.meta.env.BASE_URL}data/data.json`);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
